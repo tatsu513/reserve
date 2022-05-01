60151de9965a7f280ab23a1ab0206dc74ab62868
大川達也 <ookawatatsuya@ookawatatsuyanoMacBook-puro.local>
2022-05-01 11:55:00 +0900

    inital setting

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import "ress";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <ul>
+      <li>大川</li>
+    </ul>
+  );
+};
+
+export default Home;
